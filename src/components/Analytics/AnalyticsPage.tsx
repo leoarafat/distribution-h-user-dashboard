b9da2c7bbb8225e7d3451d8f2b89421fbc86d20b
Yeasin Arafat <leoarafat10@gmail.com>
2024-07-14 18:05:31 +0600

    m

--- src/components/Analytics/AnalyticsPage.tsx
+++ src/components/Analytics/AnalyticsPage.tsx
@@ -365,7 +365,7 @@ const AnalyticsPage = () => {
     try {
       setLoading(true);
       const response = await axios.get(
-        `http://localhost:7001/statics/analytics?month=${month}&year=${year}`,
+        `https://backend.bemusix.com/statics/analytics?month=${month}&year=${year}`,
         {
           headers: {
             Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
